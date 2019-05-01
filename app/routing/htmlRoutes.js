d40e8accbc7d975ed763444463ae992e68187531
Dan Dean <dandean218@gmail.com>
2019-05-01 01:00:22 -0500

    tweaks, v.2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,11 +3,20 @@ var path = require('path');
 
 //setting up HTML routing and makes it available to app
 module.exports = function(app){
-    app.get('/survey', function(req, res){
+
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+
+    app.get('/survey.html', function(req, res){
         res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
 
-    app.us(function(req, res){
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    app.get('/assets/style.css', function(req, res){
+        res.sendFile(path.join(__dirname + '/../assets/style.css'));
+    });
+
+    app.get('/data/friends.js', function(req, res){
+        res.sendFile(path.join(__dirname + '/../data/friends.js'));
     });
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 //setting up express and port for the app
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 9000;
 
 //middleware
 app.use(bodyParser.json());
